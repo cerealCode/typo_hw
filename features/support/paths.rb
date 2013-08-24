79530bcd1ec21d635964b43ae81a156a2538c38c
lephuocdai <lephuocdaijapan@gmail.com>
2013-08-24 14:28:09 +0000

    Part1-Scenario:Admin can merge article | Add path for Edit Page of Article with id No.

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -23,8 +23,10 @@ module NavigationHelpers
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
-
+    when /^the Edit Page of Article with id (\d+)$/
+      "/admin/content/edit/#{$1}" 
     else
+
       begin
         page_name =~ /^the (.*) page$/
         path_components = $1.split(/\s+/)
